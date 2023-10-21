ca6aa637e5e65e02f33652ddcfe568584f7e9d9f
apramodk <apramodk@vols.utk.edu>
2023-10-21 12:55:04 -0400

    new login system

---
+++ Website/src/routes/login/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { Card, Button } from 'flowbite-svelte';
+	import CenterBox from '$lib/CenterBox.svelte';
+	import InputGroup from '$lib/InputGroup.svelte';
+</script>
+
+<main>
+	<CenterBox>
+		<Card>
+			<form class="mb-6 flex flex-col space-y-4" color="dark">
+				<InputGroup inputName="Email" />
+				<InputGroup inputName="Password" />
+				<Button outline type="submit">Login</Button>
+			</form>
+		</Card>
+	</CenterBox>
+</main>
