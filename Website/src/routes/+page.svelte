18bd5b27488d266647800ec5392227f2d63081b0
apramodk <apramodk@vols.utk.edu>
2023-10-22 01:43:51 -0400

    navigation from landing page

--- Website/src/routes/+page.svelte
+++ Website/src/routes/+page.svelte
@@ -1,159 +1,164 @@
 <script>
-// @ts-nocheck
+	// @ts-nocheck
 
-
-	import { GradientButton } from "flowbite-svelte";
-
+	import { GradientButton } from 'flowbite-svelte';
 </script>
+
 <!DOCTYPE html>
 <!-- svelte-ignore a11y-missing-attribute -->
 <html>
-<head>
+	<head>
+		<style>
+			body {
+				font-family: sans-serif;
+			}
 
-    <style>
+			header {
+				background-color: #000;
+				color: #fff;
+				padding: 1rem;
+			}
 
-        body {
-            font-family:sans-serif;
-        }
+			h1,
+			h2 {
+				margin: 0;
+			}
 
-        header {
-            background-color: #000;
-            color: #fff;
-            padding: 1rem;
-        }
+			h1 {
+				font-size: 2rem;
+			}
 
-        h1, h2 {
-            margin: 0;
-        }
+			h2 {
+				font-size: 1.5rem;
+			}
 
-        h1 {
-            font-size: 2rem;
-        }
+			main {
+				padding: 1rem;
+			}
 
-        h2 {
-            font-size: 1.5rem;
-        }
+			section {
+				margin-bottom: 1rem;
+			}
 
-        main {
-            padding: 1rem;
-        }
+			h3 {
+				font-size: 1.25rem;
+			}
 
-        section {
-            margin-bottom: 1rem;
-        }
+			p {
+				font-size: 1rem;
+			}
 
-        h3 {
-            font-size: 1.25rem;
-        }
+			ul {
+				list-style-type: disc;
+				margin-left: 1rem;
+			}
 
-        p {
-            font-size: 1rem;
-        }
+			a {
+				color: #000;
+				text-decoration: none;
+			}
 
-        ul {
-            list-style-type: disc;
-            margin-left: 1rem;
-        }
+			a:hover {
+				text-decoration: underline;
+			}
 
-        a {
-            color: #000;
-            text-decoration: none;
-        }
+			footer {
+				background-color: #000;
+				color: #fff;
+				padding: 1rem;
+				text-align: center;
+			}
 
-        a:hover {
-            text-decoration: underline;
-        }
+			nav {
+				background-color: #fff;
+				border-bottom: 1px solid #ccc;
+				padding: 1rem;
+			}
 
-        footer {
-            background-color: #000;
-            color: #fff;
-            padding: 1rem;
-            text-align: center;
-        }
+			nav ul {
+				list-style-type: none;
+				margin: 0;
+				padding: 0;
+			}
 
-        nav {
-            background-color: #fff;
-            border-bottom: 1px solid #ccc;
-            padding: 1rem;
-        }
+			nav li {
+				display: inline-block;
+				margin-right: 1rem;
+			}
 
-        nav ul {
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-        }
+			nav a {
+				color: #000;
+				text-decoration: none;
+			}
 
-        nav li {
-            display: inline-block;
-            margin-right: 1rem;
-        }
+			nav a:hover {
+				text-decoration: underline;
+			}
+		</style>
+	</head>
+	<body>
+		<header>
+			<h1>Patients</h1>
+			<h2>Are you a patient looking for your patient portal?</h2>
 
-        nav a {
-            color: #000;
-            text-decoration: none;
-        }
+			<script>
+				import { GradientButton } from 'flowbite-svelte';
+			</script>
 
-        nav a:hover {
-            text-decoration: underline;
-        }
-    </style>
-</head>
-<body>
-    <header>
-        <h1>Patients</h1>
-        <h2>Are you a patient looking for your patient portal?
-        </h2>
+			<GradientButton color="pinkToOrange" href="/signup">Get Started</GradientButton>
+		</header>
 
-        <script>
-            import { GradientButton } from 'flowbite-svelte';
-          </script>
+		<nav>
+			<ul>
+				<!-- svelte-ignore a11y-invalid-attribute -->
+				<li><a href="#">Home</a></li>
+				<!-- svelte-ignore a11y-invalid-attribute -->
+				<li><a href="#">About</a></li>
+				<!-- svelte-ignore a11y-invalid-attribute -->
+				<li><a href="#">How it works</a></li>
+				<!-- svelte-ignore a11y-invalid-attribute -->
+				<li><a href="#">Benefits</a></li>
+				<!-- svelte-ignore a11y-invalid-attribute -->
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav>
 
-<GradientButton color="pinkToOrange">Get Started</GradientButton>
+		<main>
+			<section>
+				<h3>Introducing Dr.Watch</h3>
+				<p>
+					Dr.Watch is a groundbreaking new tool that uses the Terra API to leverage standardized
+					data from smartwatches for early detection of serious health issues. Our initial focus is
+					on anomaly detection for impending Alzheimer's/dementia.
+				</p>
+			</section>
+			<section>
+				<h3>How it works</h3>
+				<p>
+					Dr.Watch collects data from your smartwatch and analyzes it for signs of anomalies. These
+					anomalies can be indicative of early-stage Alzheimer's/dementia. If Dr.Watch detects any
+					anomalies, it will alert you so that you can take action.
+				</p>
+			</section>
+			<section>
+				<h3>Benefits of Dr.Watch</h3>
+				<ul>
+					<li>Early detection of Alzheimer's/dementia</li>
+					<li>
+						Peace of mind knowing that you are being monitored for early signs of
+						Alzheimer's/dementia
+					</li>
+					<li>The ability to take action if anomalies are detected</li>
+				</ul>
+			</section>
+			<section>
+				<h3>Get started today</h3>
+				<p>Sign up for a free trial of Dr.Watch today and start taking control of your health.</p>
+				<!-- svelte-ignore a11y-invalid-attribute -->
+				<a href="#">Sign up for a free trial</a>
+			</section>
+		</main>
 
-    </header>
-
-    <nav>
-        <ul>
-            <!-- svelte-ignore a11y-invalid-attribute -->
-            <li><a href="#">Home</a></li>
-            <!-- svelte-ignore a11y-invalid-attribute -->
-            <li><a href="#">About</a></li>
-            <!-- svelte-ignore a11y-invalid-attribute -->
-            <li><a href="#">How it works</a></li>
-            <!-- svelte-ignore a11y-invalid-attribute -->
-            <li><a href="#">Benefits</a></li>
-            <!-- svelte-ignore a11y-invalid-attribute -->
-            <li><a href="#">Contact</a></li>
-        </ul>
-    </nav>
-
-    <main>
-        <section>
-            <h3>Introducing Dr.Watch</h3>
-            <p>Dr.Watch is a groundbreaking new tool that uses the Terra API to leverage standardized data from smartwatches for early detection of serious health issues. Our initial focus is on anomaly detection for impending Alzheimer's/dementia.</p>
-        </section>
-        <section>
-            <h3>How it works</h3>
-            <p>Dr.Watch collects data from your smartwatch and analyzes it for signs of anomalies. These anomalies can be indicative of early-stage Alzheimer's/dementia. If Dr.Watch detects any anomalies, it will alert you so that you can take action.</p>
-        </section>
-        <section>
-            <h3>Benefits of Dr.Watch</h3>
-            <ul>
-                <li>Early detection of Alzheimer's/dementia</li>
-                <li>Peace of mind knowing that you are being monitored for early signs of Alzheimer's/dementia</li>
-                <li>The ability to take action if anomalies are detected</li>
-            </ul>
-        </section>
-        <section>
-            <h3>Get started today</h3>
-            <p>Sign up for a free trial of Dr.Watch today and start taking control of your health.</p>
-            <!-- svelte-ignore a11y-invalid-attribute -->
-            <a href="#">Sign up for a free trial</a>
-        </section>
-    </main>
-
-    <footer>
-        &copy; 2023 Dr.Watch
-    </footer>
-</body>
+		<footer>&copy; 2023 Dr.Watch</footer>
+	</body>
 </html>
